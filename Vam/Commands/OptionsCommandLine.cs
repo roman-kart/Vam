21bd7663a941c8e6cd8e85c21c8515d7a19e46d9
roman-kart <romankartasheff@gmail.com>
2022-02-14 23:14:38 +0300

    Добавил простую реализацию аргументов командной строки

---
+++ Vam/Commands/OptionsCommandLine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace Vam.Commands
+{
+    public class OptionsCommandLine
+    {
+        [Option(shortName: 'v', longName: "vam", Group = "commands", HelpText = "Start simple console editor.")]
+        public IEnumerable<string> Vam { get; set; }
+        [Option(shortName: 'c', longName: "cat", Group = "commands", HelpText = "Write all content from files")]
+        public IEnumerable<string> Cat { get; set; }
+    }
+}
--- Vam/Program.cs
+++ Vam/Program.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using CommandLine;
 using Vam.UI;
 using Vam.Files;
+using Vam.Commands;
 
 namespace Vam
 {
@@ -13,6 +15,26 @@ namespace Vam
     {
         static void Main(string[] args)
         {
+            var result = CommandLine.Parser.Default.ParseArguments<OptionsCommandLine>(args);
+            result.WithParsed(p =>
+            {
+                if (p.Cat.Count() > 0)
+                {
+                    Console.WriteLine("Cat files: ");
+                    foreach (var file in p.Cat)
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+                else if (p.Vam.Count() > 0)
+                {
+                    Console.WriteLine("Vam files: ");
+                    foreach (var file in p.Vam)
+                    {
+                        Console.WriteLine(file);
+                    }
+                }
+            });
             #region Is File Argument Valid
             // если пользователь не указал файл, то выводим сообщение об ошибке и завершаем работу программы
             if (args.Length == 0)
@@ -21,23 +43,15 @@ namespace Vam
                 Messages.WriteREADME();
                 return;
             }
-            // если пользователь хочет открыть более одного файла
-            else if (args.Length > 1)
-            {
-                Messages.WriteError("Please, write one file name.");
-                Messages.WriteREADME();
-                return;
-            }
             #endregion
 
             #region GetPathToFile
             var userPathToFile = args[0];
             var pathToFile = WorkWithFiles.GetPathToFile(userPathToFile);
-            Console.WriteLine($"Path to file: {pathToFile}");
+            //Console.WriteLine($"Path to file: {pathToFile}");
             #endregion
 
             #region Is Command Argument Valid
-
             #endregion
             //var file = WorkWithFiles.GetFile(pathToFile);
         }
